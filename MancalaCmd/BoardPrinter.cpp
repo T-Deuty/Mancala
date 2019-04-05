856519424a6ca8a57340c6f10f05dc04bf18be8e
Tyler Albert Louis Deuty <tdeuty@asu.edu>
2019-04-05 09:38:29 -0600

    game board completed

--- MancalaCmd/BoardPrinter.cpp
+++ MancalaCmd/BoardPrinter.cpp
@@ -8,26 +8,42 @@ enum Rows
 	bottomRow
 };
 
-void Board::printRows()
+string Board::buildGridString()
 {
-	// print first row, opponent then player
-	for (int i = 0; i < opponentGrid.size(); i++) {
-		cout << "[" << opponentGrid[i][topRow] << "] ";
+	string gridStr = "";
+
+	// build opponent mancala
+	gridStr.append("| |");
+	gridStr.append(25, ' ');
+	gridStr.append("| |\n|" + to_string(opponentMancala) + string("| "));
+
+	// build first row, opponent then player
+	for (size_t i = 0; i < opponentGrid.size(); i++) {
+		//cout << "[" << opponentGrid[i][topRow] << "] ";
+		gridStr += string("[") + to_string(opponentGrid[i][topRow]) + "] ";
 	}
-	for (int i = 0; i < playerGrid.size(); i++) {
-		cout << "[" << playerGrid[i][topRow] << "] ";
+	for (size_t i = 0; i < playerGrid.size(); i++) {
+		//cout << "[" << playerGrid[i][topRow] << "] ";
+		gridStr += string("[") + to_string(playerGrid[i][topRow]) + "] ";
 	}
 
 	//newline
-	cout << endl;
+	gridStr.append("|" + to_string(playerMancala) + string("|\n| | "));
 
-	// print bottom row, opponent then player
-	for (int i = 0; i < opponentGrid.size(); i++) {
-		cout << "[" << opponentGrid[i][bottomRow] << "] ";
+	// build bottom row, opponent then player
+	for (size_t i = 0; i < opponentGrid.size(); i++) {
+		//cout << "[" << opponentGrid[i][bottomRow] << "] ";
+		gridStr += string("[") + to_string(opponentGrid[i][bottomRow]) + "] ";
 	}
-	for (int i = 0; i < playerGrid.size(); i++) {
-		cout << "[" << playerGrid[i][bottomRow] << "] ";
+	for (size_t i = 0; i < playerGrid.size(); i++) {
+		gridStr += string("[") + to_string(playerGrid[i][bottomRow]) + "] ";
 	}
+
+	gridStr.append("| |\n| |");
+	gridStr.append(25, ' ');
+	gridStr.append("| |");
+
+	return gridStr;
 }
 
 void Board::printCols()
@@ -63,5 +79,9 @@ Board::~Board()
 // prints the current state of the board to the console
 void Board::printBoard()
 {
-	printRows();
+
+	// print rows
+	string boardString = buildGridString();
+
+	cout << boardString;
 }
